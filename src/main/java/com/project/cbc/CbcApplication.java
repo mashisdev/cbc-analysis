aa485ced2518cc4a215c299ef78e4261ea91ac82
manuelsrl <manuelschallerrosal@gmail.com>
2025-08-01 22:39:45 -0300

    1º commit

---
+++ src/main/java/com/project/cbc/CbcApplication.java
@@ -0,0 +1,13 @@
+package com.project.cbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cbc/CbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
